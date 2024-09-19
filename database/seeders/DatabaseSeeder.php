c04c7e03daf34753faa4e72c868de74dc7dc7bd3
Gildardo Garcia Reyes <garciareyesgildardo@gmail.com>
2024-09-19 15:34:59 -0600

    avance

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Termo;
+use App\Models\Taza;
+use App\Models\Sudadera;
+use App\Models\Pulsera;
+use App\Models\Playera;
+use App\Models\Extras;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +19,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+         User::factory(10)->create();
+        /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
-    }
+        ]); */
+
+        Termo::factory(50)->create();
+        Taza::factory(50)->create();
+        Sudadera::factory(50)->create();
+        Pulsera::factory(50)->create();
+        Playera::factory(50)->create();
+        Extras::factory(50)->create();
 }
+ }
